c211da68674f98287b38cd2360000ddf24528459
Simone Pugliese <166115038+simopugliese@users.noreply.github.com>
2025-11-10 12:49:22 +0100

    Delete Criptor package

--- Wallet/src/main/java/com/simonepugliese/Criptor/Criptor.java
+++
@@ -1,8 +0,0 @@
-package com.simonepugliese.Criptor;
-
-import com.simonepugliese.Item.Item;
-
-public abstract class Criptor {
-    public abstract Item cripta(Item item);
-    public abstract Item decripta(Item item);
-}
